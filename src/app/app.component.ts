63862bc051aa3191468d7f657adc53a52e9c148c
Alan personal <alany.diaz97@gmail.com>
2025-05-17 17:15:49 -0300

    init carousel and imgs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeadComponent } from './head/head.component';
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeadComponent,],
--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-head',
-  imports: [],
+  imports: [NgbCarouselModule],
   templateUrl: './head.component.html',
   styleUrl: './head.component.css'
 })
 export class HeadComponent {
-
+  images = [
+  "src/assets/img1.png", 
+  "./../assets/img2.png", 
+  "./../../../assets/img3.jpg"];
 }
