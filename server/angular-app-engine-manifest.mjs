7e522441ee888682b170c563f568158909dc56eb
Alan personal <alany.diaz97@gmail.com>
2025-05-18 10:40:28 -0300

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/soma',
+  basePath: '/soma/browser',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/soma/',
+  baseHref: '/soma/browser',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/soma"
+    "route": "/soma/browser"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 28208, hash: '1ad982ee4cee352258a3705103591fda1a9aa3508a6591c9a0137ab1661fdc28', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17220, hash: '81f348edaa79290455878f28216d7f6b242a3d2490297ded1319d5940370bfa1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 35706, hash: '87709c325768a0b437310537bf12d6d3473f24f8960e55608281c3404085e6b4', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 28215, hash: '73f76efedf3c1cafe4832ab2e8ea9b3e95f1b3836367d99c9062e94a40881a8f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17227, hash: 'e50dafebdfc611a3421ee8912930ae06d3dab828d89862fe9b992ce00c8dcbc1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 35713, hash: '4b19c3cb9e1b573f953812e00c9fa938ee53b13ba8f67421555cb0413c8a2530', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-JTSHUL47.css': {size: 238054, hash: 'bYumgCXwp1Q', text: () => import('./assets-chunks/styles-JTSHUL47_css.mjs').then(m => m.default)}
   },
 };
