f274b28aaddc43a5058c30853342064466cf79d2
Alan personal <alany.diaz97@gmail.com>
2025-05-19 15:44:48 -0300

    added nutritional fact section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeadComponent } from './head/head.component';
 import { ProductListComponent } from "./product-list/product-list.component";
+import { NutritionalFactComponent } from './nutritional-fact/nutritional-fact.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     HeadComponent,
     ProductListComponent,
+    NutritionalFactComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
