1e13e9f22c4ceead343dc3cb4aaf6f7590769cf9
Alan personal <alany.diaz97@gmail.com>
2025-05-20 11:35:55 -0300

    added offcanvas, preparing cart

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { NgbOffcanvas, NgbOffcanvasConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-nav',
   imports: [],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',	
+  providers: [NgbOffcanvasConfig, NgbOffcanvas],
 })
 export class NavComponent {
+  constructor(
+		config: NgbOffcanvasConfig,
+		private offcanvasService: NgbOffcanvas,
+	) {
+		// customize default values of offcanvas used by this component tree
+		config.position = 'end';
+		config.backdropClass = 'bg-info';
+		config.keyboard = false;
+	}
 
+  open(content: TemplateRef<any>) {
+		this.offcanvasService.open(content);
+	}
 }
