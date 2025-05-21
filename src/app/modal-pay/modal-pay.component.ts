a3f6f221be40ca608358caa776cd91376ed4883b
Alan personal <alany.diaz97@gmail.com>
2025-05-21 18:37:26 -0300

    Creating forms and updated form behavior

--- src/app/modal-pay/modal-pay.component.ts
+++ src/app/modal-pay/modal-pay.component.ts
@@ -1,23 +1,32 @@
 import { NgIf } from '@angular/common';
 import { Component,  inject } from '@angular/core';
-import { FormsModule, FormControl, ReactiveFormsModule, ValidatorFn, AbstractControl  } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
-import { NgbTimepickerModule, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
-import { NgbCalendar, NgbDatepickerModule, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTimepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { 
+	NgbCalendar,
+	NgbDatepickerModule,
+	NgbDateStruct,
+	NgbInputDatepickerConfig,
+	NgbDropdownModule
+} 
+	
+from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-modal-pay',
-  imports: [NgbDatepickerModule, FormsModule, NgIf, NgbTimepickerModule, ReactiveFormsModule],
+  imports: [NgbDatepickerModule, FormsModule, NgIf, NgbTimepickerModule, ReactiveFormsModule, NgbDropdownModule],
   templateUrl: './modal-pay.component.html',
-  styleUrl: './modal-pay.component.css'
+  styleUrl: './modal-pay.component.css',
+  providers : [NgbInputDatepickerConfig]
 })
 export class ModalPayComponent {
 	modalRef: any;
-	showInput : boolean=false;
 	now : boolean = false;
 	later: boolean = false;
-	ctrl : FormControl;
-
+	pickUp : boolean = false;
+	delivery: boolean = false;
+	payMethodSelected: string = "";
 	today = inject(NgbCalendar).getToday();
 	model: NgbDateStruct = this.today;
 	date: { year: number; month: number } = {year:this.today.year, month: this.today.month};
@@ -26,40 +35,52 @@ export class ModalPayComponent {
 	constructor(
 		config: NgbModalConfig, 
 		private modalService: NgbModal,
+		dateConfig: NgbInputDatepickerConfig,
 	) {
 		config.backdrop = 'static';
 		config.keyboard = false;
 		config.centered = true;
 		config.modalDialogClass
-		this.ctrl = new FormControl<NgbTimeStruct | null>(
-			{ hour: 9, minute: 0, second: 0 },
-			{validators: [this.validateTime()]}
-		  );
+		//this.ctrl = new FormControl<NgbTimeStruct | null>(
+		//	{ hour: 9, minute: 0, second: 0 }
+		//);
+
+		dateConfig.minDate = { year: this.today.year, month: this.today.month, day: this.today.day-1 };
+		dateConfig.maxDate = { year: this.today.year, month: this.today.month + 1, day: 31 };
+
+		// days that don't belong to current month are not visible
+		dateConfig.outsideDays = 'hidden';
+
+		// setting datepicker popup to close only on click outside
+		dateConfig.autoClose = 'outside';
+
+		// setting datepicker popup to open above the input
+		dateConfig.placement = ['top-start', 'top-end'];
 	}
 
 	ngOnInit(): void {
 
 	}
 
-	validateTime(): ValidatorFn {
-		return (control: AbstractControl): { [key: string]: any } | null => {
-			const value = control.value as NgbTimeStruct | null;
-			if (!value) return null;
-		
-			if (value.hour < 8) return { tooEarly: true };
-			if (value.hour > 17) return { tooLate: true };
-		
-			return null;
-		};
-	}
+
 
 	open(content: any) {
 		this.modalRef = this.modalService.open(content);
 	}
 
-	onCheck(opcion: number) {
+	whenOnCheck(opcion: number) {
 		if (opcion === 1 && this.now) this.later = false;
 		if (opcion === 2 && this.later) this.now = false;
+	}
+
+	pickUpOnCheck(opcion: number) {
+		if (opcion === 1 && this.delivery) this.pickUp = false;
+		if (opcion === 2 && this.pickUp) this.delivery = false;
+	}
+
+	payMethod(op: string) : void {
+		this.payMethodSelected = op;
+		
 	}
 
 	close(){
